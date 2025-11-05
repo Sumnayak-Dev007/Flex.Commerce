1ab1bfdce60eec7b5555d0b86143983d29b1a86b
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-11-05 22:10:29 +0530

    npm path

--- ecommerce/ecomerce/settings.py
+++ ecommerce/ecomerce/settings.py
@@ -88,7 +88,7 @@ AUTHENTICATION_BACKENDS = [
 
 TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = ['127.0.0.1',]
-NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+NPM_BIN_PATH = "/usr/local/bin/npm"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
