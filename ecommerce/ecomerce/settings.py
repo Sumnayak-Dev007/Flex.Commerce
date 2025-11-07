44d7f8a0abbace94f1cb8a76d66362b999189446
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-11-07 18:23:18 +0530

    switch dbms

--- ecommerce/ecomerce/settings.py
+++ ecommerce/ecomerce/settings.py
@@ -135,12 +135,8 @@ WSGI_APPLICATION = 'ecomerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangoproject',
-        'HOST':'localhost',
-        'USER':'root',
-        'PASSWORD':'Suman_Dev001',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
